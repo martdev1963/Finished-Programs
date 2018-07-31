e3cefb3208e557378c3f12cfe5c24526164e6209
Martín Batista <martdev1963@gmail.com>
2018-07-31 16:42:48 -0400

    ver: 3.001
    Functional programming not OOP version.

--- frontend.py
+++ frontend.py
@@ -1,93 +1,74 @@
-"""----------------------------------------------------------------------------------------------------------------------------------------
-                                        /////////////////////////////////////////////////////
-                                        /                   **FRONT-END**                   /
-                                        /                 **DOCUMENTATION**                 /
-                                        /    Martin Batista - python progrmr  07/24/2018    /
-                                        /////////////////////////////////////////////////////
-FRONT-END: tKinter GUI
-BACK-END: Sqlite3
-*FEATURES/REQUIREMENTS*
-
-A program that stores this book information:
-Title, Author
-Year, ISBN
-
-User can:
-.Show list of current record (listbox widget)
-.Search current entry
-.Add Entry
-.Update Selected
-.Delete Selected
-.Close
-
-Will be using grid method for the GUI.
----------------------------------------------------------------------------------------------------------------------------------------------"""
 from tkinter import *
-import backend  # functions
+from backend import Database
 
+database=Database("books.db")  # database object is sent to self parameter and "books.db" is sent to db parameter...
+
+class Window(object):
 
 # WRAPPER FUNCTIONS THAT CONNECT THE BUTTONS TO THE BACKEND FUNCTIONS...
 
 # this function is called by simply selecting an entry or tuple from the listbox object...its not triggered by a button like the other wrapper functions...
 # list1.bind('<<ListboxSelect>>',get_selected_row)...
 def get_selected_row(event):      # note: the curselection() method returns tuples...
-    global selected_tuple # now selected_tuple is a global variable recognized throughout the code...
-    index=list1.curselection()[0] # this trick will grab the first item with index 0 per tuple...otherwise the curselection() output will return tuples like this: (2,) (3,) etc...
-    selected_tuple=list1.get(index) # returns tuple at selected index...                                                                                          # ^ index 0 of this single tuple...
-#    return selected_tuple
-    print(event)
-    print(type(event))
-    # the following code will delete or purge the entry fields and will insert(), distribute the selected tuple into the respective entry fields thus displaying it to the user...
-    e1.delete(0,END)
-    e1.insert(END,selected_tuple[1]) # title has an index of [1], the id is the primary key and it has an index of [0]...
-    e2.delete(0,END)
-    e2.insert(END,selected_tuple[2]) # author has an index of [2]
-    e3.delete(0,END)
-    e3.insert(END,selected_tuple[3]) # year has an index of [3]
-    e4.delete(0,END)
-    e4.insert(END,selected_tuple[4]) # isbn has an index of [4]
-print(END)
+    try: # prevents IndexError exception from being thrown when user clicks an empty listbox...
+        global selected_tuple # now selected_tuple is a global variable recognized throughout the code...
+        index=list1.curselection()[0] # this trick will grab the first item with index 0 per tuple...otherwise the curselection() output will return tuples like this: (2,) (3,) etc...
+        selected_tuple=list1.get(index) # returns tuple at selected index...                                                                                          # ^ index 0 of this single tuple...
+    #    return selected_tuple
+    #    print(event)
+    #    print(type(event))
+        # the following code will delete or purge the entry fields and will insert(), distribute the selected tuple into the respective entry fields thus displaying it to the user...
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1]) # title has an index of [1], the id is the primary key and it has an index of [0]...
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2]) # author has an index of [2]
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3]) # year has an index of [3]
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4]) # isbn has an index of [4]
+    except IndexError:
+        pass
+
+#print(END)
 
 
 # b1 button...text="View All"
 def view_command():
     list1.delete(0,END) # prevents program from inserting duplicate entries over and over everytime you press View All button...
-    for row in backend.view(): # view() method returns a list of tuples...
-        list1.insert(END,row) # insert list of tuples into listbox object...
+    for row in database.view(): # view() method returns a list of tuples...
+        list1.insert(END,row) # insert list of tuples into listbox object...which will then be viewable to the user...
 
 # b2=Button(window,text="Search Entry"
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()): # stringVar objects getting strings from user entry...
+    for row in database.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()): # stringVar objects getting strings from user entry...
         list1.insert(END,row) # insert the  found strings into the listbox object...
 
 # b3=Button(window,text="Add Entry"
 def add_command():
-    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()) # insert new entry into database...
+    database.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()) # insert new entry into database...
     list1.delete(0,END) # first clear the list...
     list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())) # then display the new entry to show the user's input...
     # note the tuple being used so that the new entry display's as a row and not multiple lines...
 
-
-def delete_command():
-    backend.delete(selected_tuple[0]) # will delete the selected_tuple at index[0] for that single tuple returned...
+# b5=Button(window,text="Delete selected"
+def delete_command(): # passing global variable selected_tuple[0]...
+    database.delete(selected_tuple[0]) # will delete the selected_tuple at index[0] for that single tuple returned...
     # now you won't get the TypeError: get_selected_row() missing 1 required positional argument: 'event' cuz you are not passing the event arugment to delete_command()...
 
 
 def update_command(): # getting the updated data from the entry fields and updating the database...notice the get() methods...
-    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get()) # will delete the selected_tuple at index[0] for that single tuple returned...
-    print(selected_tuple[0],selected_tuple[1],selected_tuple[2],selected_tuple[3],selected_tuple[4])
-
-
+    database.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get()) # will delete the selected_tuple at index[0] for that single tuple returned...
+    print(selected_tuple[0],selected_tuple[1],selected_tuple[2],selected_tuple[3],selected_tuple[4]) # just prints to the console for debugging purposes...
 
 
 window=Tk()  # all code goes after here...  Tk() not tk() ---> NameError: name 'tk' is not defined
 
 window.wm_title("BookStore App - powered by Python")
 
-
+#/-------------------------------------------------------------------------------WIDGETS BEGIN HERE----------------------------------------------------------------------------------/
 # label objects and their x y coordinates...
-l0=Label(window,text="Martin Batista - Guavadream Media Software")
+l0=Label(window,text="Martin Batista Python Prgrmr - Guavadream Media Software 2018 ")
 l0.grid(row=0,column=0)
 
 l1=Label(window,text="Title")
@@ -124,7 +105,7 @@ e4.grid(row=2,column=3)
 list1=Listbox(window, height=6,width=35)
 list1.grid(row=3,column=0,rowspan=6,columnspan=2)
 
-# scrollnbar object and its x y coordinates...
+# scrollbar object and its x y coordinates...
 sb1=Scrollbar(window)
 sb1.grid(row=3,column=2,rowspan=6)
 
@@ -154,4 +135,25 @@ b5.grid(row=7,column=3)
 b6=Button(window,text="Close", width=12, command=window.destroy) # closes the app...
 b6.grid(row=8,column=3)
 
+#/---------------------------------------------------------------------------WIDGETS END HERE----------------------------------------------------------------------------------------/
+
 window.mainloop()  # all code goes before here...
+
+
+#-------------------------------------------------------------------------------------** Found Bug **---------------------------------------------------------------------------------
+"""--------------------------------------------- This occurs when calling the get_selected_row() function by clicking into the listbox object...------------------------------------------------------
+DEBUGGING OUTPUT:
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/tkinter/__init__.py", line 1702, in __call__
+    return self.func(*args)
+  File "frontend.py", line 35, in get_selected_row
+    index=list1.curselection()[0] # this trick will grab the first item with index 0 per tuple...otherwise the curselection() output will return tuples like this: (2,) (3,) etc...
+IndexError: tuple index out of range
+must write an IndexError Try block...
+** To make an executable file run this ** This creates a single executable file...otherwise you get a bunch of support files that
+are actually good for troubleshooting... include the --onefile parameter...
+pyinstaller --onefile frontend.py
+This command generates a terminal command line in the background of your GUI...to avoid this type in this other parameter called --windowed
+pyinstaller --onefile --windowed frontend.py
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
